3714b4f51eca07b4d827c7438dc36090a64e52ac
davisilva4222 <davisilva4222@gmail.com>
2018-01-03 16:02:30 -0200

    sort desc tests

--- __tests__/actions/todoActions.test.js
+++ __tests__/actions/todoActions.test.js
@@ -1,23 +1,47 @@
-import { sortAsc } from '../../src/actions/todoActions'
+import { sortAsc, sortDesc } from '../../src/actions/todoActions'
 
 describe('>>> TODO ACTION', () => {
+
+    let list
+
+    beforeEach(() => {
+        list = [
+            { _id: 3, description: 'Teste 3' },
+            { _id: 1, description: 'Teste 1' },
+            { _id: 2, description: 'Teste 2' }
+        ]
+    })
+
     describe('>> sort asc action', () => {
         it('+++ has the correct type', () => {
             const action = sortAsc([])
             expect(action.type).toBe('ORDER_ASC')
         })
+
         it('+++ has the correct payload', () => {
-            const list = [
-                { _id: 3, description: 'Teste 3' },
-                { _id: 1, description: 'Teste 1' },
-                { _id: 2, description: 'Teste 2' }
-            ]
             const listOrdered = [
                 { _id: 1, description: 'Teste 1' },
                 { _id: 2, description: 'Teste 2' },
                 { _id: 3, description: 'Teste 3' }
             ]
             const action = sortAsc(list)
+            expect(action.payload).toEqual(listOrdered)
+        })
+    })
+
+    describe('>> sort desc action', () => {
+        it('+++ has the correct type', () => {
+            const action = sortDesc([])
+            expect(action.type).toBe('ORDER_DESC')
+        })
+        
+        it('+++ has the correct payload', () => {
+            const listOrdered = [
+                { _id: 3, description: 'Teste 3' },
+                { _id: 2, description: 'Teste 2' },
+                { _id: 1, description: 'Teste 1' }
+            ]
+            const action = sortDesc(list)
             expect(action.payload).toEqual(listOrdered)
         })
     })
